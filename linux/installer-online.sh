bb7cda1717c997edc58dce5b9f74dfb1b228285c
bibijaan <v2.bibijaan@gmail.com>
2020-11-26 18:27:26 +0600

    added installer-online.sh

---
+++ linux/installer-online.sh
@@ -0,0 +1,17 @@
+if ! [ -x "$(command -v curl)" ]; then
+	echo "Curl ins't installed, installing curl"
+	sudo apt install -y curl
+fi
+
+if ! [ -x "$(command -v unzip)" ]; then
+         echo "unzip ins't installed, installing unzip"
+         sudo apt install -y unzip
+fi
+
+
+
+dir=$(mktemp -d)
+curl -L https://bibijaan.com/latest/linux -o $dir/bibijaan.zip
+unzip bibijaan.zip -d $dir/unzipped
+$dir/unzipped/$(echo -n $(ls bibijaan))/install.sh
+rm -rf $dir
