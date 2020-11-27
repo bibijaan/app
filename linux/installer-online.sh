532ee03b07bf0a4e06a51633712a9ecad4fb995a
bibijaan <v2.bibijaan@gmail.com>
2020-11-27 11:09:42 +0600

    changes linux/installer-online.sh

--- linux/installer-online.sh
+++ linux/installer-online.sh
@@ -5,11 +5,27 @@ lines() {
     echo ""
 }
 
+package() {
+    dpkg -s $1 &> /dev/null
+    if [ $? -gt 0 ]; then
+        lines "$1 isn't installed, installing $1"
+		sudo apt install -y $1
+    fi
+}
+
+package curl
+package unzip
+
 lines "Downloading zip bundle"
 
 curl -L https://bibijaan.com/latest/linux.zip -o bibijaan_temp.zip
-
 echo ""
+
+if [ ! -d "bibijaan_temp.zip" ]; then
+	lines "Couldn't download remote zip file as bibijaan_temp.zip, exiting!"
+	exit 1 || return 1
+fi
+
 lines "unzipping zip bundle"
 
 unzip bibijaan_temp.zip -d bibijaan_temp
