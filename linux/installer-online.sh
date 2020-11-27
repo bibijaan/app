412a04a379aa95781110cddfc1ce0ce86ba378a7
bibijaan <v2.bibijaan@gmail.com>
2020-11-27 10:34:11 +0600

    try linux again

--- linux/installer-online.sh
+++ linux/installer-online.sh
@@ -5,20 +5,6 @@ lines() {
     echo ""
 }
 
-lines "checking packages (curl, unzip, sqlite3, libsqlite3-dev)"
-
-package() {
-	if ! [ -x "$(command -v $1)" ]; then
-		lines "$1 isn't installed, installing $1"
-		sudo apt install -y $1
-	fi
-}
-
-package curl
-package unzip
-package sqlite3
-package libsqlite3-dev
-
 lines "Downloading zip bundle"
 
 curl -L https://bibijaan.com/latest/linux.zip -o bibijaan_temp.zip
@@ -36,9 +22,6 @@ lines "running install.sh"
 bibijaan_temp/$dirVer/install.sh
 
 echo ""
-
-lines "BiBiJAAN Desktop app should be installed!"
-lines "Version: $(cat $HOME/.local/share/applications/bibijaan.desktop | grep 'Version' | cut -d '=' -f 2)"
 
 lines "Removing downloaded files"
 
